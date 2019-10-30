3f6c303f67e7e72634f9774983e43a73cd0b536d
QueKinney <q_kinney@yahoo.com>
2019-10-29 19:21:24 -0500

    initial commit

--- src/main/java/com/example/moodjournal/controllers/WelcomeController.java
+++ src/main/java/com/example/moodjournal/controllers/WelcomeController.java
@@ -1,10 +1,15 @@
 package com.example.moodjournal.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 
+    @RequestMapping(value="")
+    @ResponseBody
     public String index(){
         return "Welcome to Mood Journal";
     }
